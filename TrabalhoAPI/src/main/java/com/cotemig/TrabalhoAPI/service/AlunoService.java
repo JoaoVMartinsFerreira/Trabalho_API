6460d27ee132dfa538efaad512b22e60e7631e1d
João Vitor Martins Ferreira <joaovitormartins20@hotmail.com>
2021-04-22 17:30:50 -0300

    Correção Banco de Dados.

--- TrabalhoAPI/src/main/java/com/cotemig/TrabalhoAPI/service/AlunoService.java
+++ TrabalhoAPI/src/main/java/com/cotemig/TrabalhoAPI/service/AlunoService.java
@@ -4,6 +4,8 @@ import com.cotemig.TrabalhoAPI.model.Aluno;
 import java.util.List;
 import java.util.Optional;
 
+
+
 public interface AlunoService {
 	 Optional<Aluno> getAlunoById(Integer id);
 	 List<Aluno> getAllAlunos();
